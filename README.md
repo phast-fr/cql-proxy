4dbec544de0147d701a444591a5a8b083b8ac1b9
David Ouagne <david.ouagne@phast.fr>
2021-10-20 09:28:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CQL Proxy
+
+[![MIT License][license-image]][license]
+
+The objective of this project is the implementation of a CQL (Clinical Quality Language) library execution service.
+
+## Related Projects
+
+[Clinical Quality Language](https://github.com/cqframework/clinical_quality_language) - Tooling in support of the CQL specification, including the CQL verifier/translator used in this project.
+
+[CQL Evaluation Engine](https://github.com/DBCG/cql_engine) - An open source Java-based evaluation engine capable of evaluating the result of any CQL expression.
+
+[license-image]: http://img.shields.io/badge/license-MIT-blue.svg
+[license]: LICENSE.md
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+
 plugins {
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
+    id("java-library")
+    id("maven-publish")
+    id("signing")
 }
 
-group = "com.phast"
+group = "fr.phast"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
@@ -19,6 +25,9 @@ configurations {
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+    }
 }
 
 dependencies {
@@ -34,6 +43,9 @@ dependencies {
 
     implementation("info.cqframework:cql-to-elm:1.5.4")
     implementation("org.opencds.cqf.cql:engine:1.5.2")
+
+    implementation("fr.phast:phast-fhir-kt:0.0.2-SNAPSHOT")
+    implementation("fr.phast:cql-engine-fhir:0.0.3-SNAPSHOT")
 }
 
 tasks.withType<KotlinCompile> {
@@ -45,4 +57,71 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            afterEvaluate {
+                artifactId = tasks.jar.get().archiveBaseName.get()
+            }
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name.set(rootProject.name)
+                packaging = "jar"
+                description.set(project.description)
+                url.set("https://github.com/phast-fr/TODO")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("davidouagne")
+                        name.set("David Ouagne")
+                        email.set("david.ouagne@phast.fr")
+                        organization.set("Phast")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/phast-fr/TODO.git")
+                    developerConnection.set("scm:git:https://github.com/phast-fr/TODO.git")
+                    url.set("https://github.com/phast-fr/TODO.git")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
