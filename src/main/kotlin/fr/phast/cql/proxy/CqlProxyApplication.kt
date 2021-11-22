2a6228ee0dcce2b2e177d3e44a49e4b253ff9f42
David Ouagne <david.ouagne@phast.fr>
2021-11-22 13:41:54 +0100

    chore(configuration): adapt configuration to platform

--- src/main/kotlin/fr/phast/cql/proxy/CqlProxyApplication.kt
+++ src/main/kotlin/fr/phast/cql/proxy/CqlProxyApplication.kt
@@ -27,9 +27,11 @@ package fr.phast.cql.proxy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableDiscoveryClient
 open class CqlProxyApplication
 
 fun main(args: Array<String>) {
