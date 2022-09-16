024ed123115210f9ec22f453a86ffe839caf6d35
David Ouagne <david.ouagne@phast.fr>
2022-09-16 16:33:27 +0200

    fix(FHIR): effective attribute for MedicationStatement

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("com.github.ben-manes.caffeine:caffeine")
 
-    implementation("fr.phast:cql-services:0.0.39-SNAPSHOT")
+    implementation("fr.phast:cql-services:0.0.40-SNAPSHOT")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
